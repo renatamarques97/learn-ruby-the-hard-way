3f2401d94b2d9372667b5be622a0b9323aada5b7
Renata Marques <renata.nrmarques@gmail.com>
2020-07-04 19:09:18 -0300

    34 added

---
+++ 34/ex34.rb
@@ -0,0 +1,28 @@
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+# animal at 1
+puts animals[1]
+
+# 3rd animal
+puts animals[2]
+
+# 1st animal
+puts animals.first
+puts animals[0]
+
+# animal at 3
+puts animals[3]
+
+# 5th animal
+puts animals[4]
+
+# animal at 2
+puts animals[2]
+
+# 6th animal
+puts animals.last
+puts animals[5]
+
+# animal at 4
+puts animals[4]
+
